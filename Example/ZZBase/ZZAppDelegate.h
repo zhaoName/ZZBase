6d5a9238b8cf418edaaf449943ab885729c54304
zhao <zhao@192.168.0.103>
2019-09-17 22:27:40 +0800

    Initial commit

---
+++ Example/ZZBase/ZZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZZAppDelegate.h
+//  ZZBase
+//
+//  Created by zhaoName on 09/17/2019.
+//  Copyright (c) 2019 zhaoName. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ZZBase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZZBase
+//
+//  Created by zhaoName on 09/17/2019.
+//  Copyright (c) 2019 zhaoName. All rights reserved.
+//
+
+@import UIKit;
+#import "ZZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZZAppDelegate class]));
+    }
+}
